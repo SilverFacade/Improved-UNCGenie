5d4477338f50885a408b75f0c6713356b53079c3
McIgo330 <99839951+MattCIgo@users.noreply.github.com>
2024-04-18 22:40:31 -0400

    Update index.js

--- front-end/src/Components/Nav/index.js
+++ front-end/src/Components/Nav/index.js
@@ -1,6 +1,5 @@
 import "./index.scss"
 import {Link} from "react-router-dom";
-import jpg1 from '../imgs/scenicPhoto.jpg';
 //import LogoS from '../../assets/images/logo-s.png'
 //import LogoSubtitle from '../../assets/images/logo_sub.png'
 
