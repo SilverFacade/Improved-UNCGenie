87aa9cbfd98ae190bc3d5ef2500d5fae503fcc1a
MattCIgo <mcigo330@gmail.com>
2024-04-29 02:55:37 -0400

    altered ClassInfo page

--- front-end/src/Components/ClassInfo/index.js
+++ front-end/src/Components/ClassInfo/index.js
@@ -42,6 +42,11 @@ const ClassInfo = () => {
             console.log(data);
             setReqs(data);
 
+            document.getElementById('classTitleTitle').innerHTML = "Course:";
+            document.getElementById('descTitle').innerHTML = "Description:";
+            document.getElementById('creditsTitle').innerHTML = "Credits:";
+            document.getElementById('requisitesTitle').innerHTML = "Prerequisites:";
+
             if (data[2]) {
                 document.getElementById('requisites').innerHTML = data[0].req_course_subject + " " + data[0].req_course_number
                     + ", " + data[1].req_course_subject + " " + data[1].req_course_number + ", " + data[2].req_course_subject + " " + data[2].req_course_number;
@@ -55,6 +60,7 @@ const ClassInfo = () => {
             }
         });
     
+        
         document.getElementById('classTitle').innerHTML = subject + " " + course_number + ":\n" + title;
         document.getElementById('desc').innerHTML = description;
         document.getElementById('credits').innerHTML = credits; 
@@ -82,7 +88,7 @@ const ClassInfo = () => {
                         {courses && courses.map((course, i) => (
                             <li key={i} onClick={(e) => 
                                 renderClassInfo(e, course.subject, course.course_number, course.credits, course.title, course.description)}>
-                                {course.title}
+                                {course.subject} {course.course_number} : {course.title}
                             </li>
                         ))}
                     </ul>
@@ -91,15 +97,16 @@ const ClassInfo = () => {
 
             <div className = {'classInfoDiv'}>
                 <div id ={'titleDiv'}>
-                    <h1>Course:</h1>
-                    <p id={'classTitle'}></p>
+                    <h1 id={'classTitleTitle'}></h1>
+                    <p id={'classTitle'}>This is the Class Information Page. Select a subject and course
+                    on the left to view information about that course.</p>
                 </div>
                 <div id ={'descDiv'}>
-                    <h2>Description:</h2>
+                    <h2 id={'descTitle'}></h2>
                     <p id={'desc'}></p>
-                    <h2>Credits:</h2>
+                    <h2 id={'creditsTitle'}></h2>
                     <p id={'credits'}></p>
-                    <h2>Prerequisites:</h2>
+                    <h2 id={'requisitesTitle'}></h2>
                     <p id={'requisites'}></p>
                 </div>
             </div>
